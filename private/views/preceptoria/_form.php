cb3873cc0de231de696b68c2a91e42fc4c0975fb
Mariano Sala <ingmsala@gmail.com>
2019-07-03 10:50:45 -0300

    Ssabados prefecptria 2

--- private/views/preceptoria/_form.php
+++ private/views/preceptoria/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Preceptoria */
@@ -10,11 +11,15 @@ use yii\widgets\ActiveForm;
 
 <div class="preceptoria-form">
 
+	<?php $listTurnos=ArrayHelper::map($turnos,'id','nombre'); ?>
+
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true]) ?>
+    
+    <?= $form->field($model, 'turno')->dropDownList($listTurnos, ['prompt'=>'Seleccionar...']); ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
